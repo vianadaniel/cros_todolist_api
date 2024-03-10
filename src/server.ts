b3987044b1350432328ed64d3194644b588f04b4
Daniel Viana de Almeida <58748927+vianadaniel@users.noreply.github.com>
2024-03-10 18:30:44 -0300

    feat: task status

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,6 @@ async function startServer() {
     connect();
 
     app.listen(PORT, () => {
-        // eslint-disable-next-line no-console
         console.log(`Service running on port ${PORT}`);
     });
 }
